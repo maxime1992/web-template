61cff94cb891a0875ed2183e7213dab22290aae1
Maxime Robert <maxime.robert1992@gmail.com>
2016-02-26 21:40:55 +0100

    fix: Add strip comment for prod env on every js file

--- gulp-tasks/assets.js
+++ gulp-tasks/assets.js
@@ -8,29 +8,34 @@ module.exports = (gulp, $) => {
 				.pipe(gulp.dest('build/html/views/')),
 
 			gulp.src('src/app/controllers/**/*.js')
+				.pipe($.if($.env.isProd, $.stripComments()))
 				.pipe($.babel())
 				.pipe(gulp.dest('build/js/controllers/')),
 
 			gulp.src('src/app/directives/**/*.js')
-				.pipe($.stripComments())
+				.pipe($.if($.env.isProd, $.stripComments()))
 				.pipe($.angularEmbedTemplates())
 				.pipe($.flatten())
 				.pipe($.babel())
 				.pipe(gulp.dest('build/js/directives/')),
 
 			gulp.src('src/app/factories/**/*.js')
+				.pipe($.if($.env.isProd, $.stripComments()))
 				.pipe($.babel())
 				.pipe(gulp.dest('build/js/factories')),
 
 			gulp.src('src/app/filters/**/*.js')
+				.pipe($.if($.env.isProd, $.stripComments()))
 				.pipe($.babel())
 				.pipe(gulp.dest('build/js/filters')),
 
 			gulp.src('src/app/mock/**/*.js')
+				.pipe($.if($.env.isProd, $.stripComments()))
 				.pipe($.if($.env.isDev, $.babel()))
 				.pipe($.if($.env.isDev, gulp.dest('build/js/mocks'))),
 
 			gulp.src('src/app/app.js')
+				.pipe($.if($.env.isProd, $.stripComments()))
 				.pipe($.babel())
 				.pipe(gulp.dest('build/js/')),
 

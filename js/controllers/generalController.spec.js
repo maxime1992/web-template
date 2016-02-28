cbda0fdca9aa810d92f11e0c33f79f829ed74eec
Maxime Robert <maxime.robert1992@gmail.com>
2016-02-28 14:25:31 +0100

    Update 2016-02-28T13:25:27.638Z

--- js/controllers/generalController.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-describe('generalController', function () {
-	it('makes sure that true is true', function (done) {
-		// You can test various properties of your main controller here..
-		expect(true).toEqual(true);
-		done();
-	});
-});
